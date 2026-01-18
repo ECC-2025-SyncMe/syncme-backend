0044ae72f604d78e486251e28a0dcb28cf0d0547
Susie <thecatver@gmail.com>
2026-01-18 18:05:15 +0900

    first init

---
+++ src/main/java/com/syncme/syncme/SyncmeApplication.java
@@ -0,0 +1,13 @@
+package com.syncme.syncme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syncme/syncme/SyncmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncme.syncme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
